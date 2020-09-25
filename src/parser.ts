d175c97bc0c924a2f4830acc2c37fe24b62a19be
Neal Beeken <nbbeeken@gmail.com>
2020-09-25 12:15:51 -0400

    feat: :sparkles: Add BSONValue class

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,12 @@
-import { bsonDescriptorFrom, BSONValue } from './bytesify'
+import { BSONValue } from './bytesify'
 import { TYPE } from './constants'
 
 export function parse(sequence: Uint8Array) {
-	return Object.fromEntries([...parse_to_map(sequence).entries()].map(([key, { value }]) => [key, value]))
+    return Object.fromEntries([...parse_to_map(sequence).entries()].map(([key, { value }]) => [key, value]))
 }
 
 export class BSONDocument extends Map<string, BSONValue> {
-	documentByteLength!: number
+    documentByteLength!: number
 }
 
 /**
@@ -14,153 +14,153 @@ export class BSONDocument extends Map<string, BSONValue> {
  * @param sequence - bson bytes
  */
 export function parse_to_map(sequence: Uint8Array, offset = 0) {
-	const bsonDocument = new BSONDocument()
+    const bsonDocument = new BSONDocument()
 
-	const view = new DataView(sequence.buffer, offset)
-	let index = 0
+    const view = new DataView(sequence.buffer, offset)
+    let index = 0
 
-	const documentSize = view.getInt32(index, true)
-	index += 4
+    const documentSize = view.getInt32(index, true)
+    index += 4
 
-	let readable = true
-	while (readable && index < documentSize) {
-		const type = view.getInt8(index)
-		index += 1
+    let readable = true
+    while (readable && index < documentSize) {
+        const type = view.getInt8(index)
+        index += 1
 
-		if (index === documentSize) {
-			if (type !== 0) {
-				throw new Error('No Document null terminator')
-			}
-			break
-		}
+        if (index === documentSize) {
+            if (type !== 0) {
+                throw new Error('No Document null terminator')
+            }
+            break
+        }
 
-		const { cString: key, size: keySize } = getCString(sequence, index)
-		index += keySize
+        const { cString: key, size: keySize } = getCString(sequence, index)
+        index += keySize
 
-		let value
-		switch (type) {
-			case 0x00:
-				readable = false
-				break
-			case TYPE.DOUBLE:
-				value = view.getFloat64(index, true)
-				index += 8
-				break
-			case TYPE.STRING: {
-				const stringLength = view.getInt32(index, true)
-				index += 4
-				value = getString(sequence, index, stringLength)
-				index += stringLength
-				break
-			}
-			case TYPE.DOCUMENT: {
-				const embeddedDoc = parse_to_map(sequence.subarray(index), index)
-				index += embeddedDoc.documentByteLength
-				value = embeddedDoc
-				break
-			}
-			case TYPE.ARRAY: {
-				const embeddedDoc = parse_to_map(sequence.subarray(index), index)
-				index += embeddedDoc.documentByteLength
-				value = mapToArray(embeddedDoc)
-				break
-			}
-			case TYPE.BINARY: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.UNDEFINED:
-				value = undefined
-				break
-			case TYPE.OBJECTID: {
-				value = sequence.subarray(index, index + 12)
-				index += 12
-				break
-			}
-			case TYPE.BOOLEAN:
-				value = !!view.getUint8(index)
-				index + 1
-				break
-			case TYPE.INT64:
-			case TYPE.UTC_DATE:
-				value = view.getBigInt64(index, true)
-				index += 8
-				break
-			case TYPE.NULL: {
-				value = null
-				break
-			}
-			case TYPE.REGEX: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.DB_POINTER: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.CODE: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.SYMBOL: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.CODE_WITH_SCOPE: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.INT32: {
-				value = view.getInt32(index, true)
-				index += 4
-				break
-			}
-			case TYPE.TIMESTAMP: {
-				value = view.getBigUint64(index, true)
-				index += 8
-				break
-			}
-			case TYPE.DECIMAL128: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.MIN_KEY: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			case TYPE.MAX_KEY: {
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-				break
-			}
-			default:
-				throw new Error(`Unsupported type 0x${type.toString(16)}`)
-		}
-		bsonDocument.set(key, bsonDescriptorFrom(value))
-	}
-	bsonDocument.documentByteLength = documentSize
-	return bsonDocument
+        let value
+        switch (type) {
+            case 0x00:
+                readable = false
+                break
+            case TYPE.DOUBLE:
+                value = view.getFloat64(index, true)
+                index += 8
+                break
+            case TYPE.STRING: {
+                const stringLength = view.getInt32(index, true)
+                index += 4
+                value = getString(sequence, index, stringLength)
+                index += stringLength
+                break
+            }
+            case TYPE.DOCUMENT: {
+                const embeddedDoc = parse_to_map(sequence.subarray(index), index)
+                index += embeddedDoc.documentByteLength
+                value = embeddedDoc
+                break
+            }
+            case TYPE.ARRAY: {
+                const embeddedDoc = parse_to_map(sequence.subarray(index), index)
+                index += embeddedDoc.documentByteLength
+                value = mapToArray(embeddedDoc)
+                break
+            }
+            case TYPE.BINARY: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.UNDEFINED:
+                value = undefined
+                break
+            case TYPE.OBJECTID: {
+                value = sequence.subarray(index, index + 12)
+                index += 12
+                break
+            }
+            case TYPE.BOOLEAN:
+                value = !!view.getUint8(index)
+                index + 1
+                break
+            case TYPE.INT64:
+            case TYPE.UTC_DATE:
+                value = view.getBigInt64(index, true)
+                index += 8
+                break
+            case TYPE.NULL: {
+                value = null
+                break
+            }
+            case TYPE.REGEX: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.DB_POINTER: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.CODE: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.SYMBOL: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.CODE_WITH_SCOPE: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.INT32: {
+                value = view.getInt32(index, true)
+                index += 4
+                break
+            }
+            case TYPE.TIMESTAMP: {
+                value = view.getBigUint64(index, true)
+                index += 8
+                break
+            }
+            case TYPE.DECIMAL128: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.MIN_KEY: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            case TYPE.MAX_KEY: {
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+                break
+            }
+            default:
+                throw new Error(`Unsupported type 0x${type.toString(16)} - ${TYPE[type]}`)
+        }
+        bsonDocument.set(key, BSONValue.from(value))
+    }
+    bsonDocument.documentByteLength = documentSize
+    return bsonDocument
 }
 
 const utfDecoder = new TextDecoder('utf8', { fatal: true })
 function getCString(sequence: Uint8Array, index: number) {
-	let nullTerm = index
-	for (; nullTerm < 256; nullTerm++) {
-		if (sequence[nullTerm] === 0) break
-	}
+    let nullTerm = index
+    for (; nullTerm < 256; nullTerm++) {
+        if (sequence[nullTerm] === 0) break
+    }
 
-	const cString = utfDecoder.decode(sequence.slice(index, nullTerm))
+    const cString = utfDecoder.decode(sequence.slice(index, nullTerm))
 
-	return { cString, size: nullTerm - index + 1 }
+    return { cString, size: nullTerm - index + 1 }
 }
 
 function getString(sequence: Uint8Array, index: number, size: number) {
-	return utfDecoder.decode(sequence.subarray(index, index + size - 1))
+    return utfDecoder.decode(sequence.subarray(index, index + size - 1))
 }
 
 function mapToArray(map: Map<string, any>) {
-	const list = []
-	for (const [key, value] of map.entries()) {
-		list[+key] = value
-	}
-	return list
+    const list = []
+    for (const [key, value] of map.entries()) {
+        list[+key] = value
+    }
+    return list
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,31 +1,21 @@
 import test from 'tape'
 import { parse, parse_to_map } from '../src/index'
 
-// function testSingleDoubleProp() {
-//     log('single prop `a` double doc test')
+test('parse `{a: 2.3}`', function (t) {
+    const bsonDocument = parse_to_map(
+        new Uint8Array([0x10, 0x00, 0x00, 0x00, 0x01, 0x61, 0x00, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x02, 0x40, 0x00])
+    )
+    t.equal(bsonDocument.documentByteLength, 0x10, `has documentByteLength of ${0x10}`)
+    t.equal(bsonDocument.size, 1, 'has 1 key value pair')
+    t.equal(bsonDocument.get('a')?.value, 2.3, 'has key `a` equaling 2.3 ')
+    t.end()
+})
 
-//     const aMapsToDouble = parse(new Uint8Array([0x10, 0x00, 0x00, 0x00, 0x01, 0x61, 0x00, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x02, 0x40, 0x00]))
-//     expect(Object.keys(aMapsToDouble).length === 1)
-//     expect(aMapsToDouble.bsonDocument.size === 1)
-//     expect(aMapsToDouble.bsonDocument.get('a') === 2.3)
-//     logObject('Prop `a` is double Doc', aMapsToDouble)
-// }
-
-// describe('Parser reads binary BSON documents', function () {
-//     it('should read an empty document', function () {
-//         const emptyDoc = parse(new Uint8Array([5, 0, 0, 0, 0]))
-//         expect(emptyDoc.documentSize).to.equal(0)
-//         logObject('Empty Doc', emptyDoc)
-//     })
-// })
-
-test('parser', function (t) {
-	//@ts-ignore
-	const bsonDocument = parse_to_map(
-		new Uint8Array([0x10, 0x00, 0x00, 0x00, 0x01, 0x61, 0x00, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x02, 0x40, 0x00])
-	)
-	t.equal(bsonDocument.documentByteLength, 0x10, `has documentByteLength of ${0x10}`)
-	t.equal(bsonDocument.size, 1, 'has 1 key value pair')
-	t.equal(bsonDocument.get('a')?.value, 2.3, 'has key `a` equaling 2.3 ')
-	t.end()
+test('parse `{}`', function (t) {
+    const bsonDocument = parse_to_map(
+        new Uint8Array([0x05, 0x00, 0x00, 0x00, 0x00])
+    )
+    t.equal(bsonDocument.documentByteLength, 5, `has documentByteLength of ${5}`)
+    t.equal(bsonDocument.size, 0, 'has 0 key value pair')
+    t.end()
 })
